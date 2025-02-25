46fc698a6efcea42b7505e694e671f5b95bafc2a
Nick (ntg) <frangiadakis@gmail.com>
2025-02-25 18:37:27 +0100

    Update go-cpu.sh: updates from server
    Signed-off-by: Nick (ntg) <frangiadakis@gmail.com>

--- go-cpu.sh
+++ go-cpu.sh
@@ -1,17 +1,16 @@
-MODEL_DIR=/home/ntg/eeas/whisper/2-get-a-docker/whisper_cache
+MODEL_DIR=/home/llmdev/code/asr-test/whisperx/whisper_cache
 
 echo MODEL_DIR: ${MODEL_DIR}
 
-echo docker run --gpus all -it \
-  -v ${MODEL_DIR}:/.cache -v ${MODEL_DIR}:/home/1001/.cache \
-  whisperx -- --model large-v2 --daemon \
-  --diarize 
-echo
-echo
-echo
-echo
-docker run --gpus all -it \
+echo docker run -d --name whisperx_cpu \
   -v ${MODEL_DIR}:/.cache -v ${MODEL_DIR}:/home/1001/.cache \
   whisperx -- --model large-v2 --daemon --device cpu --compute_type int8\
-  --diarize 
-
+  --diarize $@
+echo
+echo
+echo
+echo
+docker run -d --name whisperx_cpu \
+  -v ${MODEL_DIR}:/.cache -v ${MODEL_DIR}:/home/1001/.cache \
+  whisperx -- --model large-v2 --daemon --device cpu --compute_type int8\
+  --diarize $@
